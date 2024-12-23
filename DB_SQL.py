72b602f110e43756d5bf23a753ed19bf71e01e67
Julia Etmanova <blikdom@yandex.ru>
2024-12-23 18:30:07 +0300

    23/12 env добавлено

--- DB_SQL.py
+++ DB_SQL.py
@@ -1,11 +1,18 @@
 import psycopg2 as ps
+import os
+import dotenv
 
+dotenv.load_dotenv()
+host = os.getenv('HOST_POSTGRES_DEV2')
+user = os.getenv('USER_POSTGRES_DEV2')
+password = os.getenv('PASSWORD_POSTGRES_DEV2')
+database = os.getenv('DATABASE_POSTGRES_DEV2')
 # Создаем соединение
 connection = ps.connect(
-    host='192.168.21.3',
-    user='industrial_stg',
-    password='industrial_psw',
-    database='industrial_stg'
+    host=host,
+    user=user,
+    password=password,
+    database=database
 )
 
 cursor = connection.cursor()  # Создаем программный объект, обязательно
@@ -14,19 +21,25 @@ table_name = 'materials'
 material_ext_num = "18124"
 
 
-def get_ext_num(table,ext_num):
+def get_ext_num(table, ext_num):
+    # GET ищем в таблице  по ext_num
     request_to_read_data = f'SELECT*FROM {table} WHERE ext_num=%s'
     cursor.execute(request_to_read_data, (ext_num,))
     data = cursor.fetchall()
     for row in data:
         print(data)
-#get_ext_num(table_name,material_ext_num)
 
-def delete_ext_num(table,ext_num):
+
+get_ext_num(table_name,material_ext_num)
+
+
+def delete_ext_num(table, ext_num):
+    # Удаляем из таблицы по ext_num)
     request_to_delete_data = f'DELETE FROM {table} WHERE ext_num=%s'
     cursor.execute(request_to_delete_data, (ext_num,))
     connection.commit()
-delete_ext_num(table_name,material_ext_num)
 
+
+delete_ext_num(table_name, material_ext_num)
 cursor.close()  # выход обязательно
 connection.close()  # выход обязательно
--- test_tests.py
+++ test_tests.py
@@ -1,82 +1,96 @@
 import requests
 import allure
-BASE_URL ="http://192.168.21.3:8000"
+import os
+import dotenv
+import DB_SQL
+
+
+
+dotenv.load_dotenv()
+BASE_URL = os.getenv('BASE_URL_DEV2')
+
+
 @allure.title(" POST Создание нового материалы без кромки")
-@allure.testcase('https://team-0pkc.testit.software/projects/1/tests/3?isolatedSection=7cfd3124-54cb-490c-a03e-1a37ca875bd9','3')
+@allure.testcase(
+    'https://team-0pkc.testit.software/projects/1/tests/3?isolatedSection=7cfd3124-54cb-490c-a03e-1a37ca875bd9',
+    '3') # Ссылка на NTestIT
 def test_post_greate_mat():
-# без кромки новый материал
+    # без кромки новый материал
     payload = {
-    "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d82",
-    "ext_num": "18124",
-    "name": "18decemb",
-    "depth": 1,
-    "length": 1,
-    "width": 1,
-    "cover": "",
-    "structure": "",
-    "texture": "LONG_SIDE",
-    "edge_material_ext_ids": []
+        "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d82",
+        "ext_num": "18125",
+        "name": "18decemb",
+        "depth": 1,
+        "length": 1,
+        "width": 1,
+        "cover": "",
+        "structure": "",
+        "texture": "LONG_SIDE",
+        "edge_material_ext_ids": []
     }
     response = requests.post(f"{BASE_URL}/api/materials/", json=payload)
     print(response)
 
     assert response.status_code == 200
-    assert response.json()['result'] == True
+    assert response.json()['result'] is True
+
 
 def test_get_by_ext_num():
-    response = requests.get(f"{BASE_URL}/api/materials/by_ext_num/?ext_num=18124")
+    # проверка, что материал создан
+    response = requests.get(f"{BASE_URL}/api/materials/by_ext_num/?ext_num='18125'")
     assert response.status_code == 200
     print(response)
+
+    # удаление из базы тестовой записи
+    #DB_SQL.delete_ext_num('materials','18124')
 
 def test_post_greate_mat_edge():
-#с кромкой новый материал
+    # с кромкой новый материал
     payload = {
-    "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d88",
-    "ext_num": "18125",
-    "name": "18decembкромка",
-    "depth": 1,
-    "length": 1,
-    "width": 1,
-    "cover": "",
-    "structure": "",
-    "texture": "LONG_SIDE",
-    "edge_material_ext_ids": ["9c439966-4f36-11ea-90af-0050569c2c21", "d4771887-30d5-11e7-80f0-005056be21bb"]
+        "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d88",
+        "ext_num": "18125",
+        "name": "18decembкромка",
+        "depth": 1,
+        "length": 1,
+        "width": 1,
+        "cover": "",
+        "structure": "",
+        "texture": "LONG_SIDE",
+        "edge_material_ext_ids": ["9c439966-4f36-11ea-90af-0050569c2c21", "d4771887-30d5-11e7-80f0-005056be21bb"]
     }
     response = requests.post(f"{BASE_URL}/api/materials/", json=payload)
     print(response)
 
     assert response.status_code == 200
-    assert response.json()['result'] == True
+    assert response.json()['result'] is True
 # нужна проверка записи в таблицу material_item_edge ручной тест
 
+
 def test_post_greate_mat_not_existed_edge():
-#с кромкой новый материал
+    #  несуществующей кромкой новый материал
     payload = {
-    "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d88",
-    "ext_num": "18126",
-    "name": "18decemb кромка не существ",
-    "depth": 1,
-    "length": 1,
-    "width": 1,
-    "cover": "",
-    "structure": "",
-    "texture": "LONG_SIDE",
-    "edge_material_ext_ids": ["not existed edge"]
+        "ext_id": "940d86c7-7ad2-4b42-b304-e1444b941d88",
+        "ext_num": "18126",
+        "name": "18decemb кромка не существ",
+        "depth": 1,
+        "length": 1,
+        "width": 1,
+        "cover": "",
+        "structure": "",
+        "texture": "LONG_SIDE",
+        "edge_material_ext_ids": ["not existed edge"]
     }
     response = requests.post(f"{BASE_URL}/api/materials/", json=payload)
 
-
     assert response.status_code == 404
-    assert response.json()['result'] == False
+    assert response.json()['result'] is False
     print(response.json())
+
 
 def test_get_by_ext_num_not_existed():
     response = requests.get(f"{BASE_URL}/api/materials/by_ext_num/?ext_num=18126")
     assert response.status_code == 400
 
 
-    #delete from materials
-#where ext_num = '18124'
-
-
-
+# Удаление тестовых данных сделать через  SQL delete from materials
+# where ext_num = '18124'
